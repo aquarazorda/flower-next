9415c90255a71c81f7263f0fbe855b9bab6829d8
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-12-01 02:27:25 +0400

    moved to netlify functions for ms

--- src/server/otelms/bookings.ts
+++ src/server/otelms/bookings.ts
@@ -1,26 +1,19 @@
 import { env } from "~/env";
 import { getCurrentDate, getFutureDate } from "~/app/_lib/utils";
 import { load } from "cheerio";
-import { getOtelmsFetch } from "./auth";
 
 export const getBookedDates = async () => {
-  const { MS_BOOKINGS_URL } = env;
-  const customFetch = await getOtelmsFetch();
+  const { MS_URL } = env;
 
   const headers = new Headers();
-
   headers.append("Content-Type", "application/x-www-form-urlencoded");
-  headers.append(
-    "User-Agent",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
-  );
 
   const body = new URLSearchParams();
   body.append("date", `${getCurrentDate()} - ${getFutureDate(24)}`);
   body.append("lines_per_page", "500");
   body.append("date_type", "0");
 
-  const res = await customFetch(MS_BOOKINGS_URL, {
+  const res = await fetch(MS_URL + "/reservation_c2/rlist/1", {
     headers,
     body,
     method: "POST",
@@ -30,7 +23,6 @@ export const getBookedDates = async () => {
 };
 
 const generateBookingDates = (html: string) => {
-  console.log(html);
   const $ = load(html);
   const rows = $(".table.dataTable tr");
 
