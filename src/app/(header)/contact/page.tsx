e06e3f0ef80bdc83142b4f92ab7eaaf92c85b5e5
aquarazorda <g32.gelashvili@gmail.com>
2023-12-27 17:51:48 +0400

    fix: contact page redesign

--- src/app/(header)/contact/page.tsx
+++ src/app/(header)/contact/page.tsx
@@ -5,7 +5,15 @@ import Instagram from "~/app/_assets/icons/Instagram";
 import Phone from "~/app/_assets/icons/Phone";
 import { Button } from "~/app/_components/ui/button";
 import { Input } from "~/app/_components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableRow,
+} from "~/app/_components/ui/table";
 import { useCopyText } from "~/app/_lib/clipboard";
+import { cn } from "~/app/_lib/utils";
 import { inter } from "~/app/_styles/fonts";
 
 const info = {
@@ -17,8 +25,8 @@ export default function ContactPage() {
   const copy = useCopyText();
 
   return (
-    <div className="lg:mt-12">
-      <div className="h-96 p-6">
+    <div className="flex flex-col lg:mt-12 lg:flex-row lg:py-20">
+      <div className="h-96 flex-1 p-6 lg:h-[60dvh]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2979.436417580777!2d44.79803947643907!3d41.68951357720013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440cf2358c2f91%3A0xc0d3daaf99fd2b62!2sHotel%20Flower!5e0!3m2!1sen!2sge!4v1703108979366!5m2!1sen!2sge"
           width="100%"
@@ -29,53 +37,89 @@ export default function ContactPage() {
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
-      <div className="flex gap-4 px-6">
-        <div className="flex flex-1 gap-2">
-          <Input defaultValue={info.phoneNumber} disabled />
-          <Button variant="outline" onClick={() => copy(info.phoneNumber)}>
-            <Copy className="size-4" />
-          </Button>
-        </div>
-        <div className="flex flex-1 gap-2">
-          <Input defaultValue={info.email} disabled />
-          <Button variant="outline" onClick={() => copy(info.email)}>
-            <Copy className="size-4" />
-          </Button>
-        </div>
-      </div>
       <div
-        className={`${inter.className} my-4 flex w-full flex-wrap items-center justify-center gap-4 px-6`}
+        className={cn(
+          inter.className,
+          "flex flex-1 flex-col gap-2 px-2 lg:pr-6",
+        )}
       >
-        <Button asChild variant={"outline"} className="flex-1 gap-2">
-          <a href={`tel:${info.phoneNumber}`}>
-            <Phone style={{ width: "20px", height: "20px" }} />
-            Call
-          </a>
-        </Button>
-        <Button asChild variant={"outline"} className="flex-1 gap-2">
-          <a href="https://www.instagram.com/hotel_flower_tbilisi/">
-            <Instagram style={{ width: "20px", height: "20px" }} />
-            Instagram
-          </a>
-        </Button>
-        <Button asChild variant={"outline"} className="flex-1 gap-2">
-          <a href={`https://wa.me/${info.phoneNumber}`}>
-            <img
-              src="/icons/whatsapp.svg"
-              style={{ width: "20px", height: "20px" }}
-            />
-            Whatsapp
-          </a>
-        </Button>
-        <Button asChild variant={"outline"} className="flex-1 gap-2">
-          <a href={`https://t.me/${info.phoneNumber}`}>
-            <img
-              style={{ width: "20px", height: "20px" }}
-              src="/icons/telegram.svg"
-            />
-            Telegram
-          </a>
-        </Button>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>Phone number</TableCell>
+              <TableCell>+995 598 688 686</TableCell>
+              <TableCell>
+                <Button
+                  variant="outline"
+                  className="size-8 p-0"
+                  onClick={() => copy(info.phoneNumber)}
+                >
+                  <Copy className="size-3" />
+                </Button>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Email</TableCell>
+              <TableCell>{info.email}</TableCell>
+              <TableCell>
+                <Button
+                  variant="outline"
+                  className="size-8 p-0"
+                  onClick={() => copy(info.email)}
+                >
+                  <Copy className="size-3" />
+                </Button>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Address</TableCell>
+              <TableCell>36a Lado Asatiani Street, Tbilisi</TableCell>
+              <TableCell>
+                <Button
+                  variant="outline"
+                  className="size-8 p-0"
+                  onClick={() => copy("36a Lado Asatiani Street, Tbilisi")}
+                >
+                  <Copy className="size-3" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <div
+          className={`my-4 mt-auto flex w-full flex-wrap items-center justify-center gap-4 px-2`}
+        >
+          <Button asChild variant={"outline"} className="flex-1 gap-2">
+            <a href={`tel:${info.phoneNumber}`}>
+              <Phone style={{ width: "20px", height: "20px" }} />
+              Call
+            </a>
+          </Button>
+          <Button asChild variant={"outline"} className="flex-1 gap-2">
+            <a href="https://www.instagram.com/hotel_flower_tbilisi/">
+              <Instagram style={{ width: "20px", height: "20px" }} />
+              Instagram
+            </a>
+          </Button>
+          <Button asChild variant={"outline"} className="flex-1 gap-2">
+            <a href={`https://wa.me/${info.phoneNumber}`}>
+              <img
+                src="/icons/whatsapp.svg"
+                style={{ width: "20px", height: "20px" }}
+              />
+              Whatsapp
+            </a>
+          </Button>
+          <Button asChild variant={"outline"} className="flex-1 gap-2">
+            <a href={`https://t.me/${info.phoneNumber}`}>
+              <img
+                style={{ width: "20px", height: "20px" }}
+                src="/icons/telegram.svg"
+              />
+              Telegram
+            </a>
+          </Button>
+        </div>
       </div>
     </div>
   );
